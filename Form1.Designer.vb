7890db1d24e44e48f3da87c0deffda9e4c388333
laluhawari <laluhusnihawari@gmail.com>
2021-05-11 14:31:51 +0800

    App OderMakanana

---
+++ Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Cbkmakanan = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Cbkminuman = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnulang = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnhitung = New System.Windows.Forms.Button()
+        Me.txtminum = New System.Windows.Forms.TextBox()
+        Me.txtmakan = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnkeluar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Cbkmakanan
+        '
+        Me.Cbkmakanan.FormattingEnabled = True
+        Me.Cbkmakanan.Items.AddRange(New Object() {"Nasi Uduk", "Bakso", "Mie Ayam"})
+        Me.Cbkmakanan.Location = New System.Drawing.Point(33, 35)
+        Me.Cbkmakanan.Name = "Cbkmakanan"
+        Me.Cbkmakanan.Size = New System.Drawing.Size(121, 21)
+        Me.Cbkmakanan.TabIndex = 0
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.CheckBox1.Location = New System.Drawing.Point(33, 85)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(205, 19)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Termasuk dengan minuman"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Cbkminuman
+        '
+        Me.Cbkminuman.FormattingEnabled = True
+        Me.Cbkminuman.Items.AddRange(New Object() {"Es Jeruk", "Es Kelapa Muda", "Teh Manis"})
+        Me.Cbkminuman.Location = New System.Drawing.Point(33, 125)
+        Me.Cbkminuman.Name = "Cbkminuman"
+        Me.Cbkminuman.Size = New System.Drawing.Size(121, 21)
+        Me.Cbkminuman.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(272, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Harga  : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(271, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Harga  : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(351, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Rp. 0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(351, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Rp. 0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnulang)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.btnhitung)
+        Me.GroupBox1.Controls.Add(Me.txtminum)
+        Me.GroupBox1.Controls.Add(Me.txtmakan)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 189)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(416, 211)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Menu Pesanan"
+        '
+        'btnulang
+        '
+        Me.btnulang.BackColor = System.Drawing.Color.Gold
+        Me.btnulang.Location = New System.Drawing.Point(58, 126)
+        Me.btnulang.Name = "btnulang"
+        Me.btnulang.Size = New System.Drawing.Size(80, 28)
+        Me.btnulang.TabIndex = 13
+        Me.btnulang.Text = "Ulang"
+        Me.btnulang.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Silver
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(238, 131)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 18)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Rp. "
+        '
+        'btnhitung
+        '
+        Me.btnhitung.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnhitung.Location = New System.Drawing.Point(144, 126)
+        Me.btnhitung.Name = "btnhitung"
+        Me.btnhitung.Size = New System.Drawing.Size(80, 28)
+        Me.btnhitung.TabIndex = 12
+        Me.btnhitung.Text = "Hitung"
+        Me.btnhitung.UseVisualStyleBackColor = False
+        '
+        'txtminum
+        '
+        Me.txtminum.Location = New System.Drawing.Point(241, 81)
+        Me.txtminum.Name = "txtminum"
+        Me.txtminum.Size = New System.Drawing.Size(119, 24)
+        Me.txtminum.TabIndex = 11
+        '
+        'txtmakan
+        '
+        Me.txtmakan.Location = New System.Drawing.Point(241, 33)
+        Me.txtmakan.Name = "txtmakan"
+        Me.txtmakan.Size = New System.Drawing.Size(119, 24)
+        Me.txtmakan.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(42, 84)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(177, 18)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Berapa porsi minuman"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(42, 33)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(177, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Berapa porsi makanan"
+        '
+        'btnkeluar
+        '
+        Me.btnkeluar.BackColor = System.Drawing.Color.DarkRed
+        Me.btnkeluar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnkeluar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnkeluar.Location = New System.Drawing.Point(355, 435)
+        Me.btnkeluar.Name = "btnkeluar"
+        Me.btnkeluar.Size = New System.Drawing.Size(95, 37)
+        Me.btnkeluar.TabIndex = 13
+        Me.btnkeluar.Text = "Keluar"
+        Me.btnkeluar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(462, 484)
+        Me.Controls.Add(Me.btnkeluar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Cbkminuman)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Cbkmakanan)
+        Me.Name = "Form1"
+        Me.Text = "Order Makanan"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Cbkmakanan As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Cbkminuman As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnhitung As Button
+    Friend WithEvents txtminum As TextBox
+    Friend WithEvents txtmakan As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnkeluar As Button
+    Friend WithEvents btnulang As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Public hrgmakanan, hrgminuman As Integer
+
+    Private Sub Cbkmakanan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Cbkmakanan.SelectedIndexChanged
+        Select Case Cbkmakanan.Text
+            Case "Nasi Uduk"
+                Label3.Text = "Rp. 3500"
+                hrgmakanan = 3500
+            Case "Bakso"
+                Label3.Text = "Rp. 6000"
+                hrgmakanan = 6000
+            Case "Mie Ayam"
+                Label3.Text = "Rp. 7000"
+                hrgmakanan = 7000
+        End Select
+    End Sub
+
+
+    Private Sub Cbkminuman_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Cbkminuman.SelectedIndexChanged
+        Select Case Cbkminuman.Text
+            Case "Es Jeruk"
+                Label4.Text = "Rp. 3000"
+                hrgminuman = 3000
+            Case "Es Kelapa Muda"
+                Label4.Text = "Rp. 5000"
+                hrgminuman = 5000
+            Case "Teh Manis"
+                Label4.Text = "Rp. 6000"
+                hrgminuman = 6000
+        End Select
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+
+        InitializeComponent()
+        Cbkminuman.Visible = False
+        Label4.Visible = False
+        txtminum.Visible = False
+
+        ' Add any initialization after the InitializeComponent() call.
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = False Then
+            Cbkminuman.Visible = False
+            Label4.Visible = False
+            txtminum.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            Cbkminuman.Visible = True
+            Label4.Visible = True
+            txtminum.Visible = True
+        End If
+    End Sub
+
+    Private Sub Btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim porsimakanan, porsiminuman, jumlah As Integer
+
+        If txtmakan.Text = "" Then
+            porsimakanan = 0
+        Else
+            porsimakanan = txtmakan.Text
+            jumlah = (hrgmakanan * porsimakanan)
+            Label7.Text = "Rp. " & jumlah.ToString
+        End If
+
+        If txtminum.Text = "" Then
+            porsiminuman = 0
+        Else
+            porsimakanan = txtmakan.Text
+            porsiminuman = txtminum.Text
+
+            jumlah = (hrgmakanan * porsimakanan) + (hrgminuman * porsiminuman)
+            Label7.Text = "Rp. " & jumlah.ToString
+        End If
+    End Sub
+
+    Private Sub Btnulang_Click(sender As Object, e As EventArgs) Handles btnulang.Click
+        Cbkmakanan.Text = ""
+        Cbkminuman.Text = ""
+        txtmakan.Text = ""
+        txtminum.Text = ""
+        Label3.Text = ""
+        Label4.Text = ""
+        Label7.Text = ""
+        Cbkmakanan.Focus()
+    End Sub
+
+
+    Private Sub Btnkeluar_Click(sender As Object, e As EventArgs) Handles btnkeluar.Click
+        End
+    End Sub
+
+End Class
+
+
+
+
